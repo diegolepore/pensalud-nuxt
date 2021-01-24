33aa84ec99cf6e34fa0ebc7dd36aa74b9de95c12
admin <admin@admins-MacBook-Pro.local>
2021-01-24 01:06:34 +0100

    Removed ga custom nuxt plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,9 +24,7 @@ export default {
   css: ['@/assets/styles/reset.scss', '@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    { src: '~plugins/ga.js', mode: 'client' }
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
